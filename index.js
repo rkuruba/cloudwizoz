d24a4367f1f17409810fcf8acfd00739218d252c
Ramesh Kuruba <ramesh.kuruba@ge.com>
2019-06-26 20:24:56 -0400

    EC2 data

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var AWS = require('aws-sdk');
 // Set the region 
 AWS.config.update({region: 'us-east-1'});
 // Create S3 service object
+
 s3 = new AWS.S3({apiVersion: '2006-03-01'});
 
 // Call S3 to list the buckets
@@ -12,4 +13,14 @@ s3.listBuckets(function(err, data) {
   } else {
     console.log("Success", data.Buckets);
   }
+ 
+});
+
+// Call EC2 to list the servers
+var ec2=new AWS.EC2();
+
+ec2.describeInstances(function(err, data) {
+  if (err) console.log(err, err.stack); // an error occurred
+  else     console.log(JSON.stringify(data));           // successful response
+
 });
